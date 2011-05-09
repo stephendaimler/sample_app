0659521a317239f1344e50805dbbb03606fd71d5
Stephen Daimler <stephen@packlate.com>
2011-05-09 16:20:17 -0400

    Done with sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,21 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
     @title = "Sign up"
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      @user.password = nil
+      @user.password_confirmation = nil
+      render 'new'
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,17 +27,19 @@ class User < ActiveRecord::Base
   
   before_save :encrypt_password
 
-  # Return true if the user's password matches the submitted password.
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
-    # Compare encrypted_password with the encrypted version of
-    # submitted_password.
   end
   
   def self.authenticate(email, submitted_password)
       user = find_by_email(email)
       return nil  if user.nil?
       return user if user.has_password?(submitted_password)
+  end
+  
+  def self.authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil
   end
   
   private
